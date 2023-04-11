7d708090e65605fd83930e740d32bc22dfdfe22d
Damien Cassou <damien@cassou.me>
2023-04-11 19:59:06 +0200

    Workaround bug in indentation present in some versions of org
    https://orgmode.org/list/87v8i3y135.fsf@cassou.me
    https://git.savannah.gnu.org/cgit/emacs/org-mode.git/commit/?id=60ce29dc4
    
    The test to check the behavior is already there:
    ox-linuxmag-fr-tests-src-block-default-with-indentation. It was
    passing in some versions of Org and failing in more recent ones.

--- ox-linuxmag-fr.el
+++ ox-linuxmag-fr.el
@@ -417,8 +417,13 @@ contextual information."
      (lambda (line)
        (ox-linuxmag-fr--format-textp
         (let* ((ox-linuxmag-fr--inline-code-style "code_5f_em")
-               (text (org-export-data (org-element-parse-secondary-string line '(code) src-block)
-                                      (append '(:with-special-strings nil) info)))
+               ;; Remove leading space before parsing to avoid bug
+               ;; https://orgmode.org/list/87v8i3y135.fsf@cassou.me
+               (leading (save-match-data (and (string-match (rx bos (1+ blank)) line)
+                                              (match-string 0 line))))
+               (text (concat leading
+                             (string-trim (org-export-data (org-element-parse-secondary-string line '(code) src-block)
+                                                           (append '(:with-special-strings nil) info)))))
                (deindented-text (string-trim-left text))
                (indentation-length (- (length text) (length deindented-text))))
           (format "%s%s%s"
